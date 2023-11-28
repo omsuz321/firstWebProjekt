f639b13821d8a6f7adbad93649f13df4402a5d2e
omsuz321 <106392722+omsuz321@users.noreply.github.com>
2023-11-28 19:14:23 +0100

    performance mesurement is now possible

--- u5/PerformanceMeasurements.js
+++ u5/PerformanceMeasurements.js
@@ -3,22 +3,28 @@ function createElement(element = "div"){ //returns always a new documetn element
     return document.createElement(element);
 }
 
-function testTime(contentProperty, input = "<div>this is the <strong>default</strong> input</div>", parentNode = document.body) {
-    var newElement = document.createElement('div').appendChild(document.createElement('div'));
-    //document.querySelector(parentNode).appendChild(newElement);
-    var t0 = performance.now();
-    newElement[contentProperty] = input;
+function testTime(contentProperty, input = "<div>this is the <strong>default</strong> input</div>", n = 10000) {
+    var t0, t1, diff = 0;
+    var outerDiv;
+    var innerDiv;
+    for(var i = 0; i < n; i++){ // i crate alway a new perent and child
+        outerDiv = document.createElement('div');// i crate alway a new perent 
+        innerDiv = document.createElement('div');// and child
+        outerDiv.appendChild(innerDiv);
+        t0  = performance.now();
+        innerDiv[contentProperty] = input;
+        t1  = performance.now();
+        diff +=  (t1 - t0); 
+    }
 
-    //parentNode.appendChild(newElement);
-    return performance.now() - t0;; // elapsedTime
+    return (diff / n); // elapsedTime
 }
+
 
 
 //console.log(" innerHTML: " + testTime("innerHTML"));
 // console.log("innerTextz ${testTime(innerText)}");
 // console.log("textContent ${testTime(textContent)}");
 // console.log("outerHTML ${testTime(outerHTML)}");
-
-
 
 
