4621e62f506e2c7b79dbaf1eb474aaad1e7c4404
Ömer Süzen <omsuz321@gmail.com>
2023-12-04 23:20:20 +0100

    crated a folder for u6

---
+++ u6/test.js
@@ -0,0 +1,15 @@
+
+// console.log(Symbol.iterator.toString() + "hello ");
+let i = 0;
+function gen(){
+    console.log(i++)// replace 'variableName' with the actual variable name you want
+    }
+
+    
+
+
+    gen();
+    gen();
+
+    gen();
+
