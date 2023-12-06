d210e73a68cfb7df8ed5c228d2e560f085f4a0e6
Ömer Süzen <omsuz321@gmail.com>
2023-12-06 03:53:08 +0100

    put the proxy to its own file

--- u6/Priority.mjs
+++ u6/Priority.mjs
@@ -1,6 +1,6 @@
 import LazyTopologicalSort from "../u4/TopSort.mjs";
 
-class Priority {
+export default class Priority {
   constructor(dependencies) {
     this.dependencies = dependencies;
     this[Symbol.iterator] = () => {
@@ -18,48 +18,18 @@ class Priority {
   }
 }
 
-const handler = {
-  iterator: null, // Store the iterator here
-
-  get: function (target, key, receiver) {
-    if (key === "iteratorViaGeneraor") {
-      if (!this.iterator) {
-        this.iterator = target.iteratorViaGeneraor(); // Create the iterator if it doesn't exist
-      }
-
-      loggingTable.set([`k${loggingTable.size}`], `there are  ${target.dependencies.length - loggingTable.size} left `);
-      result.add(this.iterator.next().value);
-    }
-  },
-};
 
 const studentenLeben = new Priority( [
   [ "schlafen", "studieren" ],
   [ "essen", "studieren" ],
   [ "studieren", "prüfen" ]
-]);
+] )
 
-let loggingTable = new Map();
-let allAttributes = Object.keys(studentenLeben);
-let logger = new Proxy(studentenLeben, handler);
-let result = new Set();
-
-
-
-
-
-console.log("All Attributes:", allAttributes);
-console.assert(allAttributes.includes('dependencies'), 'Die Vorrang-Klasse sollte ein "dependencies"-Attribut haben.');
-
-
-// Test der Proxy-Logging-Funktionalität
-logger.iteratorViaGeneraor;
-console.assert(loggingTable.size > 0, 'Das Logging sollte mindestens einen Eintrag in der Tabelle haben.');
-console.assert(result.size > 0, 'Das Ergebnis-Set sollte mindestens einen Wert enthalten.');
-
-console.log(result);
-logger.iteratorViaGeneraor;
-logger.iteratorViaGeneraor;
-
-console.log(result);
+//checks if both functions work the same
+console.assert(
+  //using  "..." here results in getting all the values from the function into in array. 
+  [...studentenLeben.iteratorViaGeneraor()].toString() === 
+    [...studentenLeben[Symbol.iterator]()].toString(),
+  "iteratorViaGeneraor does not match the original iterator."
+);
 
---
+++ u6/Proxy.mjs
@@ -0,0 +1,46 @@
+import Priority from "./Priority.mjs";
+
+const handler = {
+    iterator: null, // Store the iterator here
+  
+    get: function (target, key, receiver) {
+      if (key === "iteratorViaGeneraor") {
+        if (!this.iterator) {
+          this.iterator = target.iteratorViaGeneraor(); // Create the iterator if it doesn't exist
+        }
+  
+        loggingTable.set([`k${loggingTable.size}`], `there are  ${target.dependencies.length - loggingTable.size} left `);
+        result.add(this.iterator.next().value);
+      }
+    },
+  };
+  
+  const studentenLeben = new Priority( [
+    [ "schlafen", "studieren" ],
+    [ "essen", "studieren" ],
+    [ "studieren", "prüfen" ]
+  ]);
+  
+  let loggingTable = new Map();
+  let allAttributes = Object.keys(studentenLeben);
+  let logger = new Proxy(studentenLeben, handler);
+  let result = new Set();
+  
+  
+  
+  
+  
+  console.log("All Attributes:", allAttributes);
+  console.assert(allAttributes.includes('dependencies'), 'Die Vorrang-Klasse sollte ein "dependencies"-Attribut haben.');
+  
+  
+  // Test der Proxy-Logging-Funktionalität
+  logger.iteratorViaGeneraor;
+  console.assert(loggingTable.size > 0, 'Das Logging sollte mindestens einen Eintrag in der Tabelle haben.');
+  console.assert(result.size > 0, 'Das Ergebnis-Set sollte mindestens einen Wert enthalten.');
+  
+  console.log(result);
+  logger.iteratorViaGeneraor;
+  logger.iteratorViaGeneraor;
+  
+  console.log(result);
