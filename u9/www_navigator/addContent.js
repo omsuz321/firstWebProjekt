e42cae68d9f34096f84b8c642b4098571e1e20b2
Ömer Süzen <omsuz321@gmail.com>
2023-12-13 04:02:10 +0100

    predefined how the function should work

--- u9/www_navigator/addContent.js
+++ u9/www_navigator/addContent.js
@@ -1,1 +1,8 @@
-function()
+
+//this function is supposesd to fetsch the subket and also all its sutitles and add them with an event lsiteiner to the corestonpiojng location
+//also other eventlistener shou ld be added, whe fot the subtitles so the content will be loeade when clicked 
+function sdaf ({upperMenue, decument, rightBar }){
+    
+    //here is the logic
+
+}
