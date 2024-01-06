3f2ef2fb09d77a5b46d876588caa9b0de681f4ab
Ömer Süzen <omsuz321@gmail.com>
2024-01-06 04:03:33 +0100

    added a fetch method and a "getAggregator"

--- src/u10/CommonFunctions.js
+++ src/u10/CommonFunctions.js
@@ -54,3 +54,5 @@ function addCross({ viewBox, field, stroke = "black", strokeWidth = "10" }) {
 
 
 }
+
+
--- src/u11/server.ts
+++ src/u11/server.ts
@@ -1,29 +1,42 @@
 
-
+import { DOMParser } from "https://esm.sh/linkedom";// implementaiton of dom by deno
+import { assert } from "https://deno.land/std@0.211.0/assert/mod.ts";
+import { Node } from "https://esm.sh/linkedom@0.16.6";
     type aggregator = "min" | "max" | "average"
+    const port = 8080; // Specify the port you want to use
+    Deno.serve({ port  },(_req) => new Response("Hello, world"));
 
+ 
+    
+    async function fetchData(url: string): Promise<Node[]> {
+        const decoder = new TextDecoder("utf-8");
+        const doc = await  Deno.readFile(url);
+        console.log(decoder.decode(doc));
+        const xml = new DOMParser().parseFromString(decoder.decode(doc), "text/xml")!;
+        return Array.from(xml.querySelectorAll("baustein"));               
+        
+    }
 
-
-    function fetchData(x: aggregator, url: string): Promise<Element[]> {
-        return fetch(url)
-                .then((result) => {
-                    return result.text();
-                    
-                })
-                .then((text) => {
-                    const xml = new DOMParser().parseFromString(text, "text/xml");
-                    let data = Array.from(xml.querySelectorAll("baustein"));
-                   // console.log(text);
-                    
-
-                    return data;
-                });
+    async function getAggregator(aggregator: aggregator, url: string ): Promise<number>{
+        const data = await fetchData(url);
+        if(aggregator === "min"){
+            return await 1;
+        }else if(aggregator === "max"){
+            return await 2;
+        }else if(aggregator === "average"){
+            return await 3;
+        }else{
+            return await 4;
         }
+        
+    }
+                                                                        
+console.log( await getAggregator("min", `../u10/data.xml`));
 
 
 
-        fetchData("min",  "http://127.0.0.1:5500/src/u10/data.xml");
-    //fetchData("min",  "http://localhost:63342/firstWebProjekt/src/u10/data.xml");
-
-
-                    
+//deno test --allow-net server.ts 
+/**
+ * had problems to find the rightg domParser implementation for tha
+ * the 
+ */
