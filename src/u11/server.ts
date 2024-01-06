1d3ad3c065fe6bd42413cc2db5414de499eb7b6d
Ömer Süzen <omsuz321@gmail.com>
2024-01-06 04:55:01 +0100

    finished 10.3

--- src/u11/server.ts
+++ src/u11/server.ts
@@ -1,42 +1,35 @@
-
-import { DOMParser } from "https://esm.sh/linkedom";// implementaiton of dom by deno
-import { assert } from "https://deno.land/std@0.211.0/assert/mod.ts";
+import { DOMParser } from "https://esm.sh/linkedom";
 import { Node } from "https://esm.sh/linkedom@0.16.6";
-    type aggregator = "min" | "max" | "average"
-    const port = 8080; // Specify the port you want to use
-    Deno.serve({ port  },(_req) => new Response("Hello, world"));
+import { Element } from "https://esm.sh/linkedom@0.16.6";
 
- 
-    
-    async function fetchData(url: string): Promise<Node[]> {
-        const decoder = new TextDecoder("utf-8");
-        const doc = await  Deno.readFile(url);
-        console.log(decoder.decode(doc));
-        const xml = new DOMParser().parseFromString(decoder.decode(doc), "text/xml")!;
-        return Array.from(xml.querySelectorAll("baustein"));               
-        
-    }
+type aggregator = "min" | "max" | "average";
 
-    async function getAggregator(aggregator: aggregator, url: string ): Promise<number>{
-        const data = await fetchData(url);
-        if(aggregator === "min"){
-            return await 1;
-        }else if(aggregator === "max"){
-            return await 2;
-        }else if(aggregator === "average"){
-            return await 3;
-        }else{
-            return await 4;
-        }
-        
-    }
-                                                                        
-console.log( await getAggregator("min", `../u10/data.xml`));
+async function fetchData(url: string): Promise<number[]> {
+  const decoder = new TextDecoder("utf-8");
+  const doc = await Deno.readFile(url);
+  const xml = new DOMParser().parseFromString(decoder.decode(doc), "text/xml")!;
+  return Array.from(xml.querySelectorAll("wert "), (element: Element) => parseFloat(element.textContent || '0'));
+}
 
+async function getAggregator(aggregator1: aggregator, url: string): Promise<number> {
+  const data = await fetchData(url);
 
+  if (data.length < 1) {
+    console.error("No data in document given");
+    return 0; // Handle this case according to your requirements
+  }
 
-//deno test --allow-net server.ts 
-/**
- * had problems to find the rightg domParser implementation for tha
- * the 
- */
+  if (aggregator1 === "min") {
+    return Math.min(...data);
+  } else if (aggregator1 === "max") {
+    return Math.max(...data);
+  } else if (aggregator1 === "average") {
+    const sum = data.reduce((acc, val) => acc + val, 0);
+    return sum / data.length;
+  } else {
+    console.error(`"${aggregator1}" is not a member of "aggregator"`);
+    return Infinity;
+  }
+}
+
+console.log(await getAggregator("average", `../u10/data.xml`));
